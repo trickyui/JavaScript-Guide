03f84523283563c295470993f5c878d150bd46e9
TrickyUI <trickyui>
2024-01-05 16:53:41 +0530

    Data types in javascript

---
+++ data_types/script.js
@@ -0,0 +1,29 @@
+//JavaScript data types: Object, Premitives
+
+// Premitives data types: number, string, boolean, undefined, null, symbol, biging
+
+// string data type
+let month = 'January';
+console.log(`Type of month variable is ${typeof month}`);
+
+// number data type
+month = 123;
+console.log(`Type of month variable is ${typeof month}`);
+
+// boolean data type
+month = true;
+console.log(`Type of month variable is ${typeof month}`);
+
+// undefined data type
+let year;
+console.log(`Type of year variable is ${typeof year}`);
+
+
+// Object data types: {}, []
+
+// bject data type
+year = {};
+console.log(`Type of year variable is ${typeof year}`);
+
+year = [];
+console.log(`Type of year variable is ${typeof year}`);
